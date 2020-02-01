14fb8bcd9d866d8ef6676362ccaed75a051a6bdf
Alexander1968 <lakiza_an@mail.ru>
2020-02-01 19:28:56 +0700

    1

---
+++ OlejO 3.0/Assets/Animator_Controller/Animation1.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animation1 : MonoBehaviour
+{
+    public Animator objectAnimator;
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            {
+                objectAnimator.SetBool("Play position", !objectAnimator.GetBool("Play position"));
+            }
+
+        if (Input.GetKeyDown(KeyCode.RightShift))
+            {
+                objectAnimator.SetBool("Play rotation", !objectAnimator.GetBool("Play rotation"));
+            }
+
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+            {
+                objectAnimator.SetBool("Play scale", !objectAnimator.GetBool("Play scale"));
+            }
+    }   
+}
