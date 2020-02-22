656063b95ab7718431b2a4b2caf1c641928f5abd
Pavlik50599 <60542695+Pavlik50599@users.noreply.github.com>
2020-02-22 19:05:05 +0700

    Update 2.0

--- OlejO 3.0/Assets/CoWorkingFolder/Opamusarok.cs
+++ OlejO 3.0/Assets/CoWorkingFolder/Opamusarok.cs
@@ -7,7 +7,7 @@ public class Opamusarok : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+       //Bedi s bashkoy 
     }
 
     // Update is called once per frame
